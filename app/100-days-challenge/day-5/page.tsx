855d43f4dbdab02de6e6666b36d99b0ba8de41f7
guchoo <geronimomembiela@gmail.com>
2024-02-17 12:43:04 -0300

    Day 5 of the 100 days challenge of bigdevsoon.

---
+++ app/100-days-challenge/day-5/page.tsx
@@ -0,0 +1,117 @@
+import { CiClock2 } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
+import { PiMedalThin, PiWalletThin } from "react-icons/pi";
+
+interface Recipe {
+  image: string;
+  name: string;
+  description: string;
+  totalTime: string;
+  level: string;
+  badget: string;
+  ingredients: Array<string>;
+  instructions: Array<string>;
+}
+
+const Page = () => {
+  const recipe: Recipe = {
+    image: "/assets/100-days-challenge/day-5/coffee.jpeg",
+    name: "Sweet Iced Coffee with Coconut Milk",
+    description:
+      "Sweet Iced Coffee with coconut milk is easy to make and stores well in the fridge. Make a large batch and save yourself a few trips to the coffee shop.",
+    totalTime: "10 minutes",
+    level: "Easy",
+    badget: "Under $2",
+    ingredients: [
+      "3 tablespoon Espresso coffee powder",
+      "1 cup off the boil water",
+      "1 cup Coconut milk",
+    ],
+    instructions: [
+      "Boil some water and let sit  for 1-2 minutes.",
+      "Put the coffee grounds into a cafetière and pour in the water.",
+      "Let the coffee steep for 5 minutes then slowly press down the plunger on your Cafetière.",
+      "Pour the coffee into a jug, allow to cool then chill for several hours.",
+      "Whisk in the coconut milk and condensed milk and serve over plenty of ice.",
+      "This coffee can be stored in the fridge for up to 5 days. Shake or stir again before serving.",
+    ],
+  };
+
+  return <Recipe {...recipe} />;
+};
+
+export default Page;
+
+const Recipe = ({
+  image,
+  name,
+  description,
+  totalTime,
+  level,
+  badget,
+  ingredients,
+  instructions,
+}: Recipe) => {
+  return (
+    <main className="relative flex min-h-screen antialiased max-md:flex-col md:flex-row">
+      <img
+        className="object-cover max-md:h-[35vh] max-md:min-w-full md:min-h-full md:w-1/2"
+        src={image}
+      />
+      <div className="space-y-4 bg-white px-8 py-4 md:absolute md:left-1/2 md:top-8 md:max-w-[50%] md:-translate-x-1/2">
+        <div className="flex items-center gap-2">
+          <FaStar className="text-[#ffa51b]" />
+          <FaStar className="text-[#ffa51b]" />
+          <FaStar className="text-[#ffa51b]" />
+          <FaStar className="text-[#ffa51b]" />
+          <FaStar className="text-[#ffa51b]" />
+          <span className="ml-2 ">(189)</span>
+        </div>
+        <h1 className="text-4xl font-bold">{name}</h1>
+      </div>
+      <section className="mb-8 space-y-4 p-8 text-sm md:mt-44">
+        <div className="flex gap-6">
+          <div className="flex items-center gap-2">
+            <CiClock2 className="h-6 w-6" />
+            <div>
+              <p className=" uppercase">Total time</p>
+              <p className=" font-semibold text-[#ffa51b]">{totalTime}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <PiMedalThin className="h-6 w-6" />
+            <div>
+              <p className="uppercase">Level</p>
+              <p className=" font-semibold text-[#ffa51b]">{level}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <PiWalletThin className="h-6 w-6" />
+            <div>
+              <p className=" uppercase">Badget</p>
+              <p className=" font-semibold text-[#ffa51b]">{badget}</p>
+            </div>
+          </div>
+        </div>
+        <p className="mb-8 italic">{description}</p>
+        <div className="space-y-2">
+          <h2 className="font-bold uppercase">Ingredients</h2>
+          {ingredients.map((ingredient) => (
+            <div className="flex items-center gap-2" key={ingredient}>
+              <input type="checkbox" />
+              <label>{ingredient}</label>
+            </div>
+          ))}
+        </div>
+        <div className="space-y-2">
+          <h2 className="font-bold uppercase">Instructions</h2>
+          <ol className="space-y-2">
+            {instructions.map((instruction, i) => (
+              <li key={i}>{`${i + 1}. ${instruction}`}</li>
+            ))}
+          </ol>
+        </div>
+      </section>
+    </main>
+  );
+};
