7f339f2a3c7deee4c281517753d306ac4fabcaba
guchoo <geronimomembiela@gmail.com>
2024-03-04 03:47:39 -0300

    Day 21 finished

--- app/100-days-challenge/solutions/PricingPlans.tsx
+++ app/100-days-challenge/solutions/PricingPlans.tsx
@@ -1,15 +1,26 @@
+"use client"
+import clsx from "clsx"
+import { useState } from "react"
+
 interface Plan {
   plan: string
-  pricing: number
+  pricing: {
+    monthly: number
+    annualy: number
+  }
   features: string[]
 
 }
 
 const PricingPlans = () => {
+  const [isMonthlyPrices, setIsMonthlyPrices] = useState(true);
   const plans: Plan[] = [
     {
       plan: "Professional",
-      pricing: 39.99,
+      pricing: {
+        monthly: 39.99,
+        annualy: 399.99
+      },
       features: [
         "All core features",
         "Priority support",
@@ -20,7 +31,10 @@ const PricingPlans = () => {
     },
     {
       plan: "Standard",
-      pricing: 29.99,
+      pricing: {
+        monthly: 29.99,
+        annualy: 299.99
+      },
       features: [
         "Most core features",
         "Email support",
@@ -31,7 +45,10 @@ const PricingPlans = () => {
     },
     {
       plan: "Ultimate",
-      pricing: 49.99,
+      pricing: {
+        monthly: 49.99,
+        annualy: 499.99
+      },
       features: [
         "All features, including premium",
         "24/7 priority support",
@@ -43,14 +60,19 @@ const PricingPlans = () => {
   ]
 
   return (
-    <main className="min-h-screen bg-slate-50 flex flex-col items-center gap-16 pt-8">
+    <main className="min-h-screen bg-slate-100 flex flex-col items-center justify-center gap-16 p-8">
       <div className="text-center">
         <span>Find ideal plan</span>
-        <h1 className="text-5xl">Unlock <span className="text-violet-500">Your Best-Fit</span> Plan Today</h1>
+        <h1 className="text-5xl">Unlock <span className="text-violet-500">Your Best-Fit</span> Plan Today 🔥</h1>
+        <div className="flex items-center justify-center gap-2 mt-8">
+          <span className={isMonthlyPrices ? "opacity-50" : "opacity-100"}>Billed annually</span>
+          <ToggleSwitch isActive={isMonthlyPrices} toggle={() => setIsMonthlyPrices(!isMonthlyPrices)}  />
+          <span className={isMonthlyPrices ? "opacity-100" : "opacity-50"}>Billed monthly</span>
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-3 gap-8 max-lg:grid-cols-1">
         {plans.map(p => (
-          <PricingCard key={p.plan} {...p} />
+          <PricingCard key={p.plan} {...p} isBestValue={p.plan === "Professional"} isMonthlyPrices={isMonthlyPrices} />
         ))}
       </div>
     </main>
@@ -59,25 +81,52 @@ const PricingPlans = () => {
 
 export default PricingPlans;
 
+interface PricingCardProps extends Plan {
+  isBestValue?: boolean
+  isMonthlyPrices: boolean
+}
 
-const PricingCard = ({ plan, pricing, features }: Plan) => {
+const PricingCard: React.FC<PricingCardProps> = ({ plan, pricing, features, isBestValue, isMonthlyPrices }) => {
   return (
-    <div className="rounded-xl shadow-lg flex flex-col gap-4 bg-white p-6">
-      <div>
-        <h1 className="text-2xl font-bold">{plan}</h1>
-        <span>${pricing}/month</span>
-      </div>
+    <div className={clsx(
+      "rounded-2xl shadow-lg flex flex-col gap-4 p-6 h-[600px] relative overflow-hidden hover:scale-105 duration-300 ease-out",
+      isBestValue ? "bg-gradient-to-t from-fuchsia-500 to-indigo-600 text-slate-50" : "bg-white",
+    )}>
+      {isBestValue && (
+        <div className="font-bold rotate-45 bg-red-500 absolute -right-12 text-center w-[200px] text-xs py-1.5">
+          <span>Best value</span>
+        </div>
+      )}
+      <h1 className="text-3xl font-bold">{plan}</h1>
+      <p className="text-lg font-semibold tracking-wide">${pricing[isMonthlyPrices ? "monthly" : "annualy"]}<span className="opacity-70 text-base">/month</span></p>
       <ul className="flex flex-col gap-2">
         {features.map((feature, i) => (
-          <li key={i} >
-            <div className="px-1 aspect-square bg-black rounded-full" /> 
-            {feature}
+          <li key={i} className="flex items-center gap-2">
+            <span className="font-black text-3xl">·</span>
+            <span className="opacity-70">{feature}</span>
           </li>
         ))}
       </ul>
-      <button className="mt-8 bg-yellow-400 rounded-xl py-4">
+      <button className="mt-auto rounded-xl py-4 bg-black text-slate-50 hover:bg-slate-200 hover:text-slate-900">
         Try free for 30 days
       </button>
     </div>
   )
 }
+
+const ToggleSwitch = ({ isActive, toggle }: { isActive: boolean; toggle: () => void }) => {
+  return (
+    <button
+      type="button"
+      className="relative h-[30px] w-[55px] rounded-full duration-150 ease-in bg-slate-700"
+      onClick={toggle}
+    >
+      <div
+        className={clsx(
+          "absolute top-1/2 aspect-square -translate-x-1/2 -translate-y-1/2 rounded-full bg-white px-2.5 shadow-md duration-300 ease-in-out",
+          isActive ? "left-3/4" : "left-1/4",
+        )}
+      />
+    </button>
+  );
+};
